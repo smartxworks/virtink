031daf98a83980bedf2fe2be4bb17ef8d6013912
weixiao-huang <hwx.simle@gmail.com>
2022-09-01 21:29:29 +0800

    feat(deploy): improve role generation

--- hack/generate.sh
+++ hack/generate.sh
@@ -4,13 +4,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-bash $GOPATH/src/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
+bash "$GOPATH"/src/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/smartxworks/virtink/pkg/generated github.com/smartxworks/virtink/pkg/apis \
   virt:v1alpha1 \
   --go-header-file ./hack/boilerplate.go.txt
 
+dir="deploy/helm/virtink/templates"
+
 controller-gen paths=./pkg/apis/... crd output:crd:artifacts:config=deploy/crd
-controller-gen paths=./cmd/virt-controller/... paths=./pkg/controller/... rbac:roleName=virt-controller output:rbac:artifacts:config=deploy/virt-controller webhook output:webhook:artifacts:config=deploy/virt-controller
-controller-gen paths=./cmd/virt-daemon/... paths=./pkg/daemon/... rbac:roleName=virt-daemon output:rbac:artifacts:config=deploy/virt-daemon
+controller-gen paths=./cmd/virt-controller/... paths=./pkg/controller/... rbac:roleName=virt-controller \
+  output:rbac:artifacts:config="$dir"/virt-controller \
+  webhook output:webhook:artifacts:config="$dir"/virt-controller
+controller-gen paths=./cmd/virt-daemon/... paths=./pkg/daemon/... rbac:roleName=virt-daemon \
+  output:rbac:artifacts:config="$dir"/virt-daemon
 
 go generate ./...
