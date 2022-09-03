fd306d6ca58d01b0ab68528b88d07b67bf3ca23e
weixiao-huang <hwx.simle@gmail.com>
2022-09-03 18:29:01 +0800

    feat: use kustomize to generate manifests.yaml

--- hack/generate.sh
+++ hack/generate.sh
@@ -10,25 +10,15 @@ bash "$GOPATH"/src/k8s.io/code-generator/generate-groups.sh "deepcopy,client,inf
   --go-header-file ./hack/boilerplate.go.txt
 
 dir="deploy/helm/virtink/templates"
+webhook_dir="hack/webhook"
 
 controller-gen paths=./pkg/apis/... crd output:crd:artifacts:config=deploy/crd
 controller-gen paths=./cmd/virt-controller/... paths=./pkg/controller/... rbac:roleName=virt-controller \
   output:rbac:artifacts:config="$dir"/virt-controller \
-  webhook output:webhook:artifacts:config="$dir"/virt-controller
+  webhook output:webhook:artifacts:config="$webhook_dir"
 controller-gen paths=./cmd/virt-daemon/... paths=./pkg/daemon/... rbac:roleName=virt-daemon \
   output:rbac:artifacts:config="$dir"/virt-daemon
 
-# TODO: should use a more elegant way for editing generated manifests.yaml
-replace="  name: virt-controller
-  annotations:
-    cert-manager.io/inject-ca-from: {{ .Release.Namespace }}/virt-controller-cert"
-
-# Replace newlines with literal \n, replace \ -> \/ for sed replace below
-replace="$(echo "${replace//$'\n'/\\n}" | sed "s/\//\\\\\//g")"
-
-sed -i "s/  name: mutating-webhook-configuration/$replace/g;
-  s/  name: validating-webhook-configuration/$replace/g;
-  s/name: webhook-service/name: virt-controller/g;
-  s/namespace: system/namespace: {{ .Release.Namespace }}/g" "$dir"/virt-controller/manifests.yaml
+kustomize build "$webhook_dir" > "$dir"/virt-controller/manifests.yaml
 
 go generate ./...
